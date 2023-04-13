fb6b43f56189e589a34865b7cb829e18314ff133
Bruna Fernandes <bcbsf@iscte-iul.pt>
2023-04-13 13:41:15 +0100

    aula pratica 5

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import twitter4j.*;
+
+public class Main {
+	public static void main(String[] args) {
+		String str = "ISCTE";
+		Twitter twitter = new TwitterFactory().getInstance();
+		try {
+			Query query = new Query(str);
+			QueryResult result;
+			do {
+				result = twitter.search(query);
+				List<Status> tweets = result.getTweets();
+				for (Status tweet : tweets) {
+					System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+				}
+			} while ((query = result.nextQuery()) != null);
+			System.exit(0);
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to search tweets: " + te.getMessage());
+			System.exit(-1);
+		}
+	}
+}
